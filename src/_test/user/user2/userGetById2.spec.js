412125ace991536e13d507f8cebc5501e142e7e9
Nataliia-Zhemanova <nataliia.zhemanova@gmail.com>
2024-08-16 09:31:00 +0900

    user2, user3, user4, user5 have been created

---
+++ src/_test/user/user2/userGetById2.spec.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { userGetByIdQ } = require('./queries')
+const { user} = require("./data");
+const gqlRequest = require('../gqlRequest')
+
+let respData = null;
+let postData = null
+
+describe('USER GET BY ID', () => {
+  describe('USER GET BY ID - POSITIVE TESTS', () => {
+    it('user get by id', (done) => {
+      postData = {
+        query: userGetByIdQ,
+        variables: {
+          userId: '66bacfcc007e35b541f75d6c'
+        }
+      }
+      gqlRequest(postData)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err)
+          respData = res.body.data.userGetById
+          console.log(respData);
+          expect(respData._id).eq('66bacfcc007e35b541f75d6c')
+         done()
+    })
+  })
+})
+
+  // describe('USER GET BY ID - NEGATIVE TESTS', () => {
+  //
+  // })
+})
